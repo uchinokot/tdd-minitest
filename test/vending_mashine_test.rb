794595f1ca0d29f3c679482a108f51cb7bfc1bf3
Kota Uchino <k.uchino@everyleaf.com>
2018-09-30 01:49:53 +0900

    100円以外はエラーにする

--- test/vending_mashine_test.rb
+++ test/vending_mashine_test.rb
@@ -7,4 +7,15 @@ class TestVendingMachine < Minitest::Test
     vending_machine.insert(100)
     assert_equal(VendingMachine.push_button, 'コーラ')
   end
+
+  def test_insert
+    vending_machine = VendingMachine.new
+    result = vending_machine.insert(100)
+    assert_equal(result, 100)
+
+    vending_machine = VendingMachine.new
+    assert_raises(InvalidCoinError) do
+      vending_machine.insert(200)
+    end
+  end
 end
