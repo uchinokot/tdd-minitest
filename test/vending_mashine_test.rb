8be909b8f67aa6a8541ad419ebee0bf10fa5c6f7
Kota Uchino <k.uchino@everyleaf.com>
2018-09-29 23:55:56 +0900

    投入メソッドを追加してテストをredにする

--- test/vending_mashine_test.rb
+++ test/vending_mashine_test.rb
@@ -4,6 +4,7 @@ require './lib/vending_machine'
 class TestVendingMachine < Minitest::Test
   def test_push_button
     vending_machine = VendingMachine.new
+    vending_machine.insert(100)
     assert_equal(vending_machine.push_button, 'コーラ')
   end
 end
