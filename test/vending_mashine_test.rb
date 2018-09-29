240b721990ffa66f732f316328e4bcd38ba92433
Kota Uchino <k.uchino@everyleaf.com>
2018-09-30 01:59:20 +0900

    ウーロン茶が出ることを期待する（テストred）

--- test/vending_mashine_test.rb
+++ test/vending_mashine_test.rb
@@ -5,7 +5,15 @@ class TestVendingMachine < Minitest::Test
   def test_push_button
     vending_machine = VendingMachine.new
     vending_machine.insert(100)
-    assert_equal(vending_machine.push_button, 'コーラ')
+    assert_equal(vending_machine.push_button('コーラ'), 'コーラ')
+
+    vending_machine = VendingMachine.new
+    vending_machine.insert(100)
+    assert_equal(vending_machine.push_button('ウーロン茶'), 'ウーロン茶')
+
+    vending_machine = VendingMachine.new
+    vending_machine.insert(100)
+    assert_equal(vending_machine.push_button('炭酸水'), '炭酸水')
   end
 
   def test_insert
