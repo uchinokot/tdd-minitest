69901fe66e56d2efa58d9f64cf1699f55cc0dc4c
Kota Uchino <k.uchino@everyleaf.com>
2018-09-30 15:40:17 +0900

    テストコードをリファクタ

--- test/vending_mashine_test.rb
+++ test/vending_mashine_test.rb
@@ -2,43 +2,34 @@ require 'minitest/autorun'
 require './lib/vending_machine'
 
 class TestVendingMachine < Minitest::Test
+  def setup
+    @vending_machine = VendingMachine.new
+  end
+
   def test_push_button
-    vending_machine = VendingMachine.new
-    vending_machine.insert(100)
-    assert_equal('コーラ', vending_machine.push_button('コーラ'))
-
-    vending_machine = VendingMachine.new
-    vending_machine.insert(100)
-    assert_equal('ウーロン茶', vending_machine.push_button('ウーロン茶'))
-
-    vending_machine = VendingMachine.new
-    vending_machine.insert(100)
-    assert_equal('炭酸水', vending_machine.push_button('炭酸水'))
+    @vending_machine.insert(100)
+    assert_equal('コーラ', @vending_machine.push_button('コーラ'))
+    assert_equal('ウーロン茶', @vending_machine.push_button('ウーロン茶'))
+    assert_equal('炭酸水', @vending_machine.push_button('炭酸水'))
   end
 
   def test_insert
-    vending_machine = VendingMachine.new
-    result = vending_machine.insert(100)
+    result = @vending_machine.insert(100)
     assert_equal(100, result)
 
-    vending_machine = VendingMachine.new
     assert_raises(InvalidCoinError) do
-      vending_machine.insert(200)
+      @vending_machine.insert(200)
     end
   end
 
   def test_total_insert_price
-    vending_machine = VendingMachine.new
-    vending_machine.insert(100)
-    vending_machine.insert(100)
-    total_inserted_price = vending_machine.total_inserted_price
+    @vending_machine.insert(100)
+    @vending_machine.insert(100)
+    total_inserted_price = @vending_machine.total_inserted_price
     assert_equal(200, total_inserted_price)
 
-    vending_machine = VendingMachine.new
-    vending_machine.insert(100)
-    vending_machine.insert(100)
-    vending_machine.insert(100)
-    total_inserted_price = vending_machine.total_inserted_price
+    @vending_machine.insert(100)
+    total_inserted_price = @vending_machine.total_inserted_price
     assert_equal(300, total_inserted_price)
   end
 end
